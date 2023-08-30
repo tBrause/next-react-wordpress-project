a27602d463b714ce869fa5f45b3001aa3ddc5e5e
tBrause <brause.torsten@gmail.com>
2023-08-30 18:19:16 +0200

    del rest and add comment

--- types/blog-types.ts
+++ types/blog-types.ts
@@ -1,31 +1,4 @@
-export type BlogPostRest = {
-	id: number;
-	date: string;
-	slug: string;
-	title: {
-		rendered: string;
-	};
-	excerpt: {
-		rendered: string;
-	};
-	content: {
-		rendered: string;
-	};
-	featured_media: number;
-};
-
-export type BlogImageRest = {
-	id: number;
-	guid: {
-		rendered: string;
-	};
-	alt_text: string;
-	media_details: {
-		width: number;
-		height: number;
-	};
-};
-
+// TYPES for WP-Blog-Entries
 type BlogPostBaseGql = {
 	id: number;
 	title: string;
